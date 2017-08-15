82bcc15533a440fd5b1dcdf55ea085801db17076
PoweredByME <saadkwi12@hotmail.com>
2017-08-15 19:10:33 +0500

    Saad : Started making the tArray ADT

--- Algorithms/tArray.cs
+++ Algorithms/tArray.cs
@@ -2,6 +2,26 @@
 
 namespace Algorithms
 {
+	public class tArrayNode<T>{
+
+		class tArrayNode_listNode<t>{
+			T item;
+			tArrayNode_listNode<t> next;
+			public tArrayNode_listNode(t item, tArrayNode_listNode<t> next){
+				this.next = next;
+				this.item = item;
+			}
+			public t Item(){ return item;}
+			public tArrayNode_listNode<t> Next(){return next;}
+		}
+
+		T item;
+		tArrayNode_listNode<T> head;
+		tArrayNode<T>[] table;
+		public tArrayNode(){}
+
+	}
+
 	public class tArray
 	{
 		public tArray ()
